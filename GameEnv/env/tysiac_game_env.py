e48664fcdb12aa0e2d2f3b0b27d7863a7fae4376
HoutarouOreki <thezjarek@gmail.com>
2024-04-29 06:28:21 +0200

    pave way for training

--- GameEnv/env/tysiac_game_env.py
+++ GameEnv/env/tysiac_game_env.py
@@ -1,11 +1,11 @@
 import functools
 import random
-from typing import NamedTuple, Tuple, Any, Dict
+from typing import NamedTuple, Any
 
+import gymnasium
 import numpy as np
-
-from pettingzoo import AECEnv
 from gymnasium import spaces
+from pettingzoo import AECEnv
 from pettingzoo.utils import agent_selector
 
 
@@ -13,16 +13,38 @@ class Card(NamedTuple):
     value: str
     suit: str
 
+    def as_number(self) -> int:
+        number = 0
+        values = "ATKQJ9"
+        suits = "♥♦♣♠"
+        number += suits.index(self.suit) * len(values) + values.index(self.value)
+        return number
+
+    def __str__(self):
+        return self.value + self.suit
+
+
+def card_from_number(number) -> Card:
+    values = "ATKQJ9"
+    suits = "♥♦♣♠"
+    value_number = number % len(values)
+    suit_number = number // len(values)
+    return Card(suit=suits[suit_number], value=values[value_number])
+
+
+def _card_arr_to_str(cards: list[Card]) -> str:
+    return " ".join([card.__str__() for card in cards])
+
 
 class TysiacGameEnv(AECEnv):
     metadata = {
         "name": "tysiac_game_env_v0",
     }
 
-    def __init__(self):
+    def __init__(self, render_mode=None):
+        self.render_mode = render_mode
         super().__init__()
         self.observations = None
-        self._agent_selector = None
         self.widow_cards: list[Card] = []
         self.a_cards: list[Card] = []
         self.b_cards: list[Card] = []
@@ -33,22 +55,36 @@ class TysiacGameEnv(AECEnv):
         self.laid_cards: list[Card] = []
         self.current_turn_cards: list[tuple[str, Card]] = []
         self.possible_agents = ["a", "b", "c"]
+        self.last_turn = {}
+        self._agent_selector = agent_selector(self.possible_agents)
 
     def reset(self, seed=None, options=None):
         self.agents = self.possible_agents.copy()
-        self._agent_selector = agent_selector(self.agents)
         self.agent_selection = self._agent_selector.next()
-        self.observations = {agent: None for agent in self.agents}
         self._cumulative_rewards = {agent: 0 for agent in self.agents}
         self.terminations = {agent: False for agent in self.agents}
         self.truncations = {agent: False for agent in self.agents}
-        self.infos = {agent: {} for agent in self.agents}
+        self.infos: dict = {agent: {} for agent in self.agents}
 
         self.a_cards = []
         self.b_cards = []
         self.c_cards = []
         self.laid_cards = []
         self.current_turn_cards = []
+
+        self.observations = self._get_empty_actors_dict({})
+        self.observations["a"] = {
+            "my_cards": np.array([x.as_number() for x in self.a_cards], dtype=np.int8),
+            "laid_cards": np.array([x.as_number() for x in self.laid_cards], dtype=np.int8)
+        }
+        self.observations["b"] = {
+            "my_cards": np.array([x.as_number() for x in self.b_cards], dtype=np.int8),
+            "laid_cards": np.array([x.as_number() for x in self.laid_cards], dtype=np.int8)
+        }
+        self.observations["c"] = {
+            "my_cards": np.array([x.as_number() for x in self.c_cards], dtype=np.int8),
+            "laid_cards": np.array([x.as_number() for x in self.laid_cards], dtype=np.int8)
+        }
 
         card_values = "ATKQJ9"
         card_suits = "♠♥♦♣"
@@ -57,7 +93,7 @@ class TysiacGameEnv(AECEnv):
 
         for card_suit in card_suits:
             for card_value in card_values:
-                deck.append(card_suit + card_value)
+                deck.append(Card(suit=card_suit, value=card_value))
 
         for agent_cards in (self.a_cards, self.b_cards, self.c_cards):
             for _ in range(7):
@@ -70,6 +106,8 @@ class TysiacGameEnv(AECEnv):
 
         self.widow_cards = [deck[x] for x in picked_cards if x is False]
 
+        self._set_observations()
+
     def step(self, action) -> tuple[Any, dict[Any, float], dict[Any, bool], dict[Any, bool], dict[Any, dict[str, Any]]]:
         if self.terminations[self.agent_selection] or self.truncations[self.agent_selection]:
             # handles stepping an agent which is already dead
@@ -80,64 +118,83 @@ class TysiacGameEnv(AECEnv):
 
         agent = self.agent_selection
 
-        if agent == "a":
-            self.laid_cards.append(self.a_cards.pop(action))
-
-        if agent == "b":
-            self.laid_cards.append(self.b_cards.pop(action))
-
-        if agent == "c":
-            self.laid_cards.append(self.c_cards.pop(action))
+        self._lay_card(agent, action)
 
         self.rewards = self._get_empty_actors_dict(0)
 
         if len(self.current_turn_cards) == len(self.agents):
             winner = self._get_winner_for_current_turn()
-            self.rewards[winner] = self._get_current_turn_points()
-            winner_index = self.agents.index(winner)
-            for _ in range(winner_index):
-                agent = self.agents.pop(0)
-                self.agents.append(agent)
+            points = self._get_current_turn_points()
+            self.rewards[winner] = points
+            self.last_turn = {
+                "winner": winner,
+                "points": points,
+                "played_cards": self.current_turn_cards.copy()
+            }
+            self.infos["turn_completed"] = True
+            self.current_turn_cards.clear()
+
+            if winner == "a":
+                self._agent_selector.reinit(["a", "b", "c"])
+            elif winner == "b":
+                self._agent_selector.reinit(["b", "c", "a"])
+            elif winner == "c":
+                self._agent_selector.reinit(["c", "a", "b"])
+        else:
+            self.infos["turn_completed"] = False
+
+        self.agent_selection = self._agent_selector.next()
 
         next_action_mask = np.zeros(7, dtype=np.int8)
         for i in range(len(self.a_cards) - 1):
             next_action_mask[i] = 1
 
-        self.observations = self._get_empty_actors_dict({})
-        self.observations["a"] = {
-            "a_cards": self.a_cards,
-            "laid_cards": self.laid_cards
-        }
-        self.observations["b"] = {
-            "b_cards": self.b_cards,
-            "laid_cards": self.laid_cards
-        }
-        self.observations["c"] = {
-            "c_cards": self.c_cards,
-            "laid_cards": self.laid_cards
-        }
+        self._set_observations()
 
         self.terminations = self._get_empty_actors_dict(len(self.a_cards) == 0)
         self.truncations = self._get_empty_actors_dict(False)
-        self.infos: dict = self._get_empty_actors_dict({})
         self.infos[agent]["action_mask"] = next_action_mask
 
-        self.agent_selection = self._agent_selector.next()
-
         return self.observations, self.rewards, self.terminations, self.truncations, self.infos
+
+    def _lay_card(self, agent, action):
+        agent_cards = self.a_cards
+        if agent == "b":
+            agent_cards = self.b_cards
+        elif agent == "c":
+            agent_cards = self.c_cards
+
+        card = agent_cards.pop(action)
+        self.laid_cards.append(card)
+        self.current_turn_cards.append((agent, card))
+
+    def _set_observations(self):
+        self.observations = self._get_empty_actors_dict({})
+        self.observations["a"] = {
+            "a_cards": np.array([x.as_number() for x in self.a_cards], dtype=np.int8),
+            "laid_cards": np.array([x.as_number() for x in self.laid_cards], dtype=np.int8)
+        }
+        self.observations["b"] = {
+            "b_cards": np.array([x.as_number() for x in self.b_cards], dtype=np.int8),
+            "laid_cards": np.array([x.as_number() for x in self.laid_cards], dtype=np.int8)
+        }
+        self.observations["c"] = {
+            "c_cards": np.array([x.as_number() for x in self.c_cards], dtype=np.int8),
+            "laid_cards": np.array([x.as_number() for x in self.laid_cards], dtype=np.int8)
+        }
 
     def _get_empty_actors_dict(self, initial_value):
         return {agent: initial_value for agent in self.agents}
 
     def _get_winner_for_current_turn(self) -> str:
-        suit: str = self.current_turn_cards[0][1][0]
+        suit: str = self.current_turn_cards[0][1].suit
 
         highest_points: int = 0
         winner: str = self.current_turn_cards[0][0]
         for (agent, card) in self.current_turn_cards:
             agent: str
-            card: str
-            points = "9JQKTA".index(card[1]) if card[0] == suit else 0
+            card: Card
+            points = "9JQKTA".index(card.value) if card.suit == suit else 0
             if points > highest_points:
                 winner = agent
 
@@ -146,27 +203,41 @@ class TysiacGameEnv(AECEnv):
     def _get_current_turn_points(self) -> int:
         points = 0
         for (agent, card) in self.current_turn_cards:
-            if card[1] in "ATK":
+            if card.value in "ATK":
                 points += 10
 
         return points
 
     def render(self):
-        pass
+        if self.render_mode is None:
+            gymnasium.logger.warn(
+                "You are calling render method without specifying any render mode."
+            )
+            return
 
+        if len(self.agents) > 0:
+            string = (f"Current state: \n\t"
+                      f"a: {_card_arr_to_str(self.a_cards)}\n\t"
+                      f"b: {_card_arr_to_str(self.b_cards)}\n\t"
+                      f"c: {_card_arr_to_str(self.c_cards)}\n")
+            if "turn_completed" in self.infos and self.infos["turn_completed"]:
+                string += (f"Last turn: \n\t"
+                           f"winner: {self.last_turn['winner']}\n\t"
+                           f"points: {self.last_turn['points']}\n\t"
+                           f"played cards: "
+                           f"{' | '.join([f'{x[0]}: {x[1].__str__()}' for x in self.last_turn['played_cards']])}\n")
+        else:
+            string = "Game over"
+        print(string)
+
+    @functools.lru_cache(maxsize=None)
     def observation_space(self, agent):
         return spaces.Dict({
-            f"{agent}_cards": spaces.Sequence(
-                spaces.Tuple((
-                    spaces.Text(1, charset="ATKQJ9"),
-                    spaces.Text(1, charset="♠♥♦♣"))
-                ),
+            f"my_cards": spaces.Sequence(
+                spaces.Discrete(24)
             ),
             "laid_cards": spaces.Sequence(
-                spaces.Tuple((
-                    spaces.Text(1, charset="ATKQJ9"),
-                    spaces.Text(1, charset="♠♥♦♣"))
-                ),
+                spaces.Discrete(24)
             )
         })
 
@@ -177,8 +248,8 @@ class TysiacGameEnv(AECEnv):
     def observe(self, agent):
         """
         Observe should return the observation of the specified agent. This function
-        should return a sane observation (though not necessarily the most up to date possible)
+        should return a sane observation (though not necessarily the most up-to-date possible)
         at any time after reset() is called.
         """
         # observation of one agent is the previous state of the other
-        return np.array(self.observations[agent])
+        return self.observations[agent]
--- main.py
+++ main.py
@@ -1,9 +1,18 @@
+import numpy as np
+from keras import Sequential
+from keras.src.layers import Flatten, Dense
+from keras.src.optimizers import Adam
+from rl.agents import DQNAgent
+from rl.memory import SequentialMemory
+from rl.policy import BoltzmannQPolicy
+
 from GameEnv.env.tysiac_game_env import TysiacGameEnv
 
 
 def start():
-    env = TysiacGameEnv()
+    env = TysiacGameEnv(render_mode="h")
     env.reset()
+    env.render()
 
     for agent in env.agent_iter():
         observation, reward, termination, truncation, info = env.last()
@@ -18,10 +27,66 @@ def start():
             action = env.action_space(agent).sample(mask)  # this is where you would insert your policy
 
         observation, reward, termination, truncation, info = env.step(action)
-        print(observation)
+        if "turn_completed" in info and info["turn_completed"]:
+            env.render()
+
+    env.close()
+
+
+def build_model(states, actions):
+    model = Sequential()
+    model.add(Flatten(input_shape=(1, states)))
+    model.add(Dense(24, activation="relu"))
+    model.add(Dense(24, activation="relu"))
+    model.add(Dense(actions, activation="linear"))
+    return model
+
+
+def build_agent(model, actions):
+    policy = BoltzmannQPolicy()
+    memory = SequentialMemory(limit=50000, window_length=1)
+    return DQNAgent(model=model, memory=memory, policy=policy,
+                    nb_actions=actions, nb_steps_warmup=10, target_model_update=1e-2)
+
+
+def train():
+    env = TysiacGameEnv(render_mode="h")
+    env.reset()
+
+    states = 2
+    actions = 7
+    print(actions)
+    print(actions)
+    model = build_model(states, actions)
+    agents = {}
+    for agent_id in env.possible_agents:
+        agent = build_agent(model, actions)
+        agent.compile(Adam(lr=1e-3), metrics=["mae"])
+        agents[agent_id] = agent
+
+    env.render()
+
+    for agent in env.agent_iter():
+        observation, reward, termination, truncation, info = env.last()
+
+        if termination or truncation:
+            action = None
+        else:
+            if "action_mask" in info:
+                mask = info["action_mask"]
+            else:
+                mask = None
+            print(env.observations[agent])
+            action = agents[agent].forward(env.observations[agent])
+            print(agent, action)
+            # action = env.action_space(agent).sample(mask)  # this is where you would insert your policy
+
+        observation, reward, termination, truncation, info = env.step(action)
+        if "turn_completed" in info and info["turn_completed"]:
+            env.render()
 
     env.close()
 
 
 if __name__ == '__main__':
-    start()
+    train()
